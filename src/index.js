474c8b905cd97120393a02e464aed27d189b000f
megletify <burrellmegane@gmail.com>
2022-01-09 19:09:05 -0600

    Integrated geolocation API and the Open Weather App API.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,55 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = (timestamp = date.getHours());
+  let minutes = date.getMinutes();
+  let months = [
+    "January",
+    "Feburary",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[date.getMonth()];
+  let day = date.getDate();
+  let year = date.getFullYear();
+  return `${month} ${day}, ${year} @ ${hours}:${minutes}`;
+}
 
+function displayTemperature(response) {
+  console.log(response.data);
+  let temp = document.querySelector("#temp");
+  temp.innerHTML = Math.round(response.data.main.temp);
+
+  let city = document.querySelector("#current-city");
+  city.innerHTML = response.data.name;
+
+  let description = document.querySelector("#description");
+  description.innerHTML = response.data.weather[0].description;
+
+  let humidity = document.querySelector("#humidity");
+  humidity.innerHTML = response.data.main.humidity;
+
+  let windSpeed = document.querySelector("#wind-speed");
+  windSpeed.innerHTML = Math.round(response.data.wind.speed);
+
+  let date = document.querySelector("#date");
+  date.innerHTML = formatDate(response.data.dt * 1000);
+
+  let icon = document.querySelector("#weather-icon");
+  icon.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+}
+let city = "Birmingham";
+let apiKey = "f6ac59a63eb6c11f9c97a0f9c5fb1033";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+
+axios.get(apiUrl).then(displayTemperature);
